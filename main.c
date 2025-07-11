3b98721a1cb038ba98d68027575d01e5c6f03b61
豊田蒼空 <152574478+SoraToyoda@users.noreply.github.com>
2025-07-11 10:29:34 +0900

    コマンドライン引数のチェックを追加し、引数が不正な場合にエラーメッセージを表示するように修正

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
+#include <stdio.h>
 #include "ui.h"
 #include "deck.h"
 #include "game.h"
 #include "input.h"
 #include "help.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 1) {
+        fprintf(stderr, "エラー: ./solitaire で実行して下さい\n");
+        return 1;
+    }
     init_ui();
     while (1) {
         int choice = draw_menu();
